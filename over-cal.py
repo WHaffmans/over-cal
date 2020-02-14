bfc2e42308276cdadace3604bb9a88ceddc235b4
WHaffmans <11960527+WHaffmans@users.noreply.github.com>
2020-02-14 18:08:39 +0100

    main, input etc

--- over-cal.py
+++ over-cal.py
@@ -4,65 +4,70 @@ from datetime import datetime, timedelta
 from icalendar import Calendar, Event, vCalAddress, vText
 import pytz
 
+def main():
+    rest = 'https://abc.over.nu/over/data/rest.php/'
+    sessionurl = rest + 'Session'
+    calurl = rest + 'ContactAgenda'
+    appurl= rest + 'ResPersonAppointment'
+
+    username = input('username:')
+    password = input('password:')
+    weeks = int(input('Hoeveel weken wil je downloaden?'))
 
 
+    payload = {"username": username, "password": password}
+    session = requests.post(sessionurl, data=json.dumps(payload))
+    userid = requests.get(sessionurl, cookies=session.cookies).json()['id']
+    calres = requests.get(calurl, cookies=session.cookies, params={'id':userid}).json()
 
-rest = 'https://abc.over.nu/over/data/rest.php/'
-sessionurl = rest + 'Session'
-calurl = rest + 'ContactAgenda'
+    appointments = calres['appointment']['item']
+    dates = calres['appointment']['date']
+    app_details = dict()
+    for id,item in appointments.items():
+        app_detail = requests.get(appurl, cookies=session.cookies, params={'id':id}).json()['item']
+        app_details[id] = app_detail
+    worktypes = ['','Afspraak:','Bijles:','Begeleiding']
 
-username = input('username:')
-password = input('password:')
+    from_date = datetime.now()
+    till_date = from_date + timedelta(weeks=weeks)
 
-payload = {"username": username, "password": password}
-session = requests.post(sessionurl, data=json.dumps(payload))
-userid = '1200'
-calres = requests.get(calurl, cookies=session.cookies, params={'id':userid}).json()
-appointments = calres['appointment']['item']
-dates = calres['appointment']['date']
-
-appurl= rest + 'ResPersonAppointment'
-app_details = dict()
-for id,item in appointments.items():
-    app_detail = requests.get(appurl, cookies=session.cookies, params={'id':id}).json()['item']
-    app_details[id] = app_detail
-worktypes = ['','Afspraak:','Bijles:','Begeleiding']
-
-from_date = datetime.now()
-till_date = from_date + timedelta(weeks=1)
-
-cal = Calendar()
-cal.add('prodid', 'OVER Rooster')
-cal.add('version','2.0')
+    cal = Calendar()
+    cal.add('prodid', 'OVER Rooster')
+    cal.add('version','2.0')
 
 
-for date, ids in sorted(dates.items()):
-    date = datetime.strptime(date,'%Y-%m-%d')
-    if len(ids) > 0 and date>from_date and date<till_date:
-        datestr = date.strftime('%Y-%m-%d')
-        for id in ids:
-            event = Event()
-            start = app_details[id]['starton_time']
-            startdate = datetime.strptime(datestr +' '+start, '%Y-%m-%d %H:%M')
-            end = app_details[id]['endon_time']
-            enddate = datetime.strptime(datestr +' '+end, '%Y-%m-%d %H:%M')
+    for date, ids in sorted(dates.items()):
+        date = datetime.strptime(date,'%Y-%m-%d')
+        if len(ids) > 0 and date>from_date and date<till_date:
+            datestr = date.strftime('%Y-%m-%d')
+            for id in ids:
+                start = app_details[id]['starton_time']
+                startdate = datetime.strptime(datestr +' '+start, '%Y-%m-%d %H:%M')
+                end = app_details[id]['endon_time']
+                enddate = datetime.strptime(datestr +' '+end, '%Y-%m-%d %H:%M')
 
-            desc = app_details[id]['description']
-            worktype = worktypes[int(app_details[id]['typeid'])]
-            people = []
-            for person in app_details[id]['person']:
-                if person['id'] not in [userid, '1765']:
-                    people.append(person['text'])
+                desc = app_details[id]['description']
+                worktype = worktypes[int(app_details[id]['typeid'])]
+                people = []
+                for person in app_details[id]['person']:
+                    if person['id'] not in [userid, '1765']:
+                        people.append(person['text'])
+                
+                event = Event()
 
-            event.add('summary', f'{worktype} {desc}')
-            event.add('dtstamp', from_date)
-            event.add('dtstart', startdate)
-            event.add('dtend', enddate)
-            #event.add('dtstamp', datetime(2005,4,4,0,10,0,tzinfo=pytz.utc))
+                event.add('summary', f'{worktype} {desc}')
+                event.add('dtstamp', from_date)
+                event.add('dtstart', startdate)
+                event.add('dtend', enddate)
+                event.add('dtstamp', datetime.now())
 
-            print(start + ' - ' + end,worktype,', '.join(people), desc)
-            cal.add_component(event)
+                cal.add_component(event)
+    write_ics(cal)
 
-f = open('rooster.ics','wb')
-f.write(cal.to_ical())
-f.close()
+def write_ics(cal):
+    f = open('rooster.ics','wb')
+    f.write(cal.to_ical())
+    f.close()
+
+if __name__ == "__main__":
+    main()
