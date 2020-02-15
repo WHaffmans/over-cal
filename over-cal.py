83b533305fb1748dde7e80a2d2a8d6d8279fad7e
WHaffmans <11960527+WHaffmans@users.noreply.github.com>
2020-02-15 01:46:56 +0100

    fix flake8 problems

--- over-cal.py
+++ over-cal.py
@@ -1,17 +1,18 @@
 import requests
 import json
 from datetime import datetime, timedelta
-from icalendar import Calendar, Event, vCalAddress, vText
+from icalendar import Calendar, Event, vText
+
 
 def main():
     rest = 'https://abc.over.nu/over/data/rest.php/'
     sessionurl = rest + 'Session'
     calurl = rest + 'ContactAgenda'
-    appurl= rest + 'ResPersonAppointment'
+    appurl = rest + 'ResPersonAppointment'
     contacturl = rest + 'ResContactHeader'
 
     try:
-        with open('config.json','r') as fh:
+        with open('config.json', 'r') as fh:
             config = json.load(fh)
             username = config['username']
             password = config['password']
@@ -22,47 +23,59 @@ def main():
         weeks = int(input('Hoeveel weken wil je downloaden?'))
         config = {
             'username': username,
-            'password': password, 
+            'password': password,
             'weeks': weeks
             }
         with open('config.json', 'w') as fh:
-            json.dump(config,fh)
-    
+            json.dump(config, fh)
+
     payload = {"username": username, "password": password}
     session = requests.post(sessionurl, data=json.dumps(payload))
     userid = requests.get(sessionurl, cookies=session.cookies).json()['id']
 
-    calres = requests.get(calurl, cookies=session.cookies, params={'id':userid}).json()
+    calres = requests.get(
+        calurl,
+        cookies=session.cookies,
+        params={'id': userid}
+        ).json()
 
     appointments_item = calres['appointment']['item']
     appointment_dates = calres['appointment']['date']
     app_details = dict()
     locations = dict()
-    for id,item in appointments_item.items():
-        app_detail = requests.get(appurl, cookies=session.cookies, params={'id':id}).json()
+    for id, item in appointments_item.items():
+        app_detail = requests.get(
+            appurl,
+            cookies=session.cookies,
+            params={'id': id}
+            ).json()
         app_details[id] = app_detail['item']
         if len(locations) == 0:
             for location in app_detail['location']:
                 locations[location['id']] = location['fullname']
-    worktypes = ['','Afspraak:','Bijles:','Begeleiding']
+    worktypes = ['', 'Afspraak:', 'Bijles:', 'Begeleiding']
 
     from_date = datetime.now()
     till_date = from_date + timedelta(weeks=weeks)
 
     cal = Calendar()
     cal.add('prodid', 'OVER Rooster')
-    cal.add('version','2.0')
+    cal.add('version', '2.0')
     contacts = dict()
 
     for date, ids in sorted(appointment_dates.items()):
-        date = datetime.strptime(date,'%Y-%m-%d')
-        if len(ids) > 0 and date>from_date and date<till_date:
+        date = datetime.strptime(date, '%Y-%m-%d')
+        if len(ids) > 0 and date > from_date and date < till_date:
             datestr = date.strftime('%Y-%m-%d')
             for id in ids:
                 start = app_details[id]['starton_time']
-                startdate = datetime.strptime(datestr +' '+start, '%Y-%m-%d %H:%M')
+                startdate = datetime.strptime(
+                    datestr + ' ' + start, '%Y-%m-%d %H:%M'
+                    )
                 end = app_details[id]['endon_time']
-                enddate = datetime.strptime(datestr +' '+end, '%Y-%m-%d %H:%M')
+                enddate = datetime.strptime(
+                    datestr + ' ' + end, '%Y-%m-%d %H:%M'
+                    )
 
                 desc = app_details[id]['description']
                 worktype = worktypes[int(app_details[id]['typeid'])]
@@ -79,25 +92,29 @@ def main():
                     if person['id'] not in [userid, '1765']:
                         if person['id'] not in contacts:
                             contact = requests.get(
-                                contacturl, 
-                                cookies=session.cookies, 
-                                params={'contactid':person['id']}
-                                ).json()['contact']   
+                                contacturl,
+                                cookies=session.cookies,
+                                params={'contactid': person['id']}
+                                ).json()['contact']
                             contacts[person['id']] = contact
                         else:
                             contact = contacts[person['id']]
-                        people += f"{contact['fullname']}, {contact['mobile']}, {contact['email']}\n"
+                        people += f"{contact['fullname']}, "
+                        people += f"{contact['mobile']}, "
+                        people += f"{contact['email']}\n"
 
-                event.add('description',vText(people))
-                event.add('location',locations[app_details[id]['locationid']])
+                event.add('description', vText(people))
+                event.add('location', locations[app_details[id]['locationid']])
                 cal.add_component(event)
-    
-    write_ics(cal,from_date, username)
+
+    write_ics(cal, from_date, username)
+
 
 def write_ics(cal, from_date, username):
-    f = open(f'{from_date.strftime("%Y-%m-%d")}_Rooster_{username}.ics','wb')
+    f = open(f'{from_date.strftime("%Y-%m-%d")}_Rooster_{username}.ics', 'wb')
     f.write(cal.to_ical())
     f.close()
+
 
 if __name__ == "__main__":
     main()
