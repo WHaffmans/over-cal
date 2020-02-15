c7a7eb4eaadf28a5ee14d1d79c7540bfc772c589
WHaffmans <11960527+WHaffmans@users.noreply.github.com>
2020-02-15 12:02:54 +0100

    rename config and url, refactor getConfig, login

--- over-cal.py
+++ over-cal.py
@@ -5,48 +5,31 @@ from icalendar import Calendar, Event, vText
 
 
 def main():
-    rest_base_url = 'https://abc.over.nu/over/data/rest.php/'
-    session_url = rest_base_url + 'Session'
-    calendar_url = rest_base_url + 'ContactAgenda'
-    appointment_url = rest_base_url + 'ResPersonAppointment'
-    contact_url = rest_base_url + 'ResContactHeader'
+    rest_url = 'https://abc.over.nu/over/data/rest.php/'
+    url = {
+        'session':      rest_url + 'Session',
+        'calendar':     rest_url + 'ContactAgenda',
+        'appointment':  rest_url + 'ResPersonAppointment',
+        'contact':      rest_url + 'ResContactHeader'
+    }
+    config = getConfig()
+    from_date = datetime.now()
+    till_date = from_date + timedelta(weeks=config['weeks'])
 
-    try:
-        with open('config.json', 'r') as fh:
-            config = json.load(fh)
-            username = config['username']
-            password = config['password']
-            weeks_to_download = config['weeks']
-    except FileNotFoundError:
-        username = input('username:')
-        password = input('password:')
-        weeks_to_download = int(input('Hoeveel weken wil je downloaden?'))
-        config = {
-            'username': username,
-            'password': password,
-            'weeks': weeks_to_download
-            }
-        with open('config.json', 'w') as fh:
-            json.dump(config, fh)
+    userid, session = login(config, url)
 
-    login_data = {"username": username, "password": password}
-    session = requests.post(session_url, data=json.dumps(login_data))
-    userid = requests.get(session_url, cookies=session.cookies).json()['id']
-
-    calres = requests.get(
-        calendar_url,
-        cookies=session.cookies,
+    appointments = requests.get(
+        url['calendar'],
+        cookies=session,
         params={'id': userid}
-        ).json()
+        ).json()['appointment']
 
-    appointments_item = calres['appointment']['item']
-    appointment_dates = calres['appointment']['date']
     app_details = dict()
     locations = dict()
-    for id, item in appointments_item.items():
+    for id, item in appointments['item'].items():
         app_detail = requests.get(
-            appointment_url,
-            cookies=session.cookies,
+            url['appointment'],
+            cookies=session,
             params={'id': id}
             ).json()
         app_details[id] = app_detail['item']
@@ -55,15 +38,12 @@ def main():
                 locations[location['id']] = location['fullname']
     worktypes = ['', 'Afspraak:', 'Bijles:', 'Begeleiding']
 
-    from_date = datetime.now()
-    till_date = from_date + timedelta(weeks=weeks_to_download)
-
     calendar = Calendar()
     calendar.add('prodid', '-//OVER Rooster//NOSGML//NL')
     calendar.add('version', '2.0')
     contacts = dict()
 
-    for date, ids in sorted(appointment_dates.items()):
+    for date, ids in sorted(appointments['date'].items()):
         date = datetime.strptime(date, '%Y-%m-%d')
         if len(ids) > 0 and date > from_date and date < till_date:
             datestr = date.strftime('%Y-%m-%d')
@@ -92,8 +72,8 @@ def main():
                     if person['id'] not in [userid, '1765']:
                         if person['id'] not in contacts:
                             contact = requests.get(
-                                contact_url,
-                                cookies=session.cookies,
+                                url['contact'],
+                                cookies=session,
                                 params={'contactid': person['id']}
                                 ).json()['contact']
                             contacts[person['id']] = contact
@@ -107,13 +87,41 @@ def main():
                 event.add('location', locations[app_details[id]['locationid']])
                 calendar.add_component(event)
 
-    write_ics(calendar, from_date, username)
+    write_ics(calendar, from_date, config['username'])
+
+
+def getConfig():
+    try:
+        with open('config.json', 'r') as fh:
+            config = json.load(fh)
+
+    except FileNotFoundError:
+        config = {
+            'username': input('username:'),
+            'password': input('password:'),
+            'weeks': int(input('Hoeveel weken wil je downloaden?'))
+            }
+
+        with open('config.json', 'w') as fh:
+            json.dump(config, fh)
+
+    return config
 
 
 def write_ics(cal, from_date, username):
     f = open(f'{from_date.strftime("%Y-%m-%d")}_Rooster_{username}.ics', 'wb')
     f.write(cal.to_ical())
     f.close()
+
+
+def login(config, url):
+    login_data = {
+        "username": config['username'],
+        "password": config['password']
+        }
+    session = requests.post(url['session'], data=json.dumps(login_data))
+    userid = requests.get(url['session'], cookies=session.cookies).json()['id']
+    return userid, session.cookies
 
 
 if __name__ == "__main__":
